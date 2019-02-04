c3a02886e3941f7fdef2455d272d517d0173e418
Dev Jana <dev@primeacademy.io>
2019-02-04 10:11:58 -0600

    end of first lecture

--- client.js
+++ client.js
@@ -1,5 +1,26 @@
 $( document ).ready( readyNow );
 
+function addEmployee(){
+    console.log( 'in addEmployee' );
+    // create a new employee object with user input
+    let newEmployee = {
+        firstName: $( '#firstNameIn' ).val(),
+        lastName: $( '#lastNameIn' ).val(),
+        id: $( '#idIn' ).val(),
+        title: $( '#titleIn' ).val(),
+        annualSalary: $( '#annualSalaryIn' ).val()
+    } // end newEmployee
+    console.log( 'newEmployee:', newEmployee );
+    // clear input fields
+    $( '#firstNameIn' ).val( '' );
+    $( '#lastNameIn' ).val( '' );
+    $( '#idIn' ).val( '' );
+    $( '#titleIn' ).val( '' );
+    $( '#annualSalaryIn' ).val( '' );
+    // push that employee into an array
+    // calculate monthly salary
+} // end add Employee
+
 function readyNow(){
-    console.log( 'JQ' );
+    $( '#addEmployeeButton' ).on( 'click', addEmployee );
 } // end readyNow
