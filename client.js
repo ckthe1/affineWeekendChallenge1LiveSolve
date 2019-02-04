179575b06db65eedc347fd83218c44780e1d1612
Dev Jana <dev@primeacademy.io>
2019-02-04 10:32:53 -0600

    total monthly calculation

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 $( document ).ready( readyNow );
 
+let employees = [];
+
 function addEmployee(){
     console.log( 'in addEmployee' );
     // create a new employee object with user input
@@ -18,7 +20,22 @@ function addEmployee(){
     $( '#titleIn' ).val( '' );
     $( '#annualSalaryIn' ).val( '' );
     // push that employee into an array
+    employees.push( newEmployee );
     // calculate monthly salary
+    let totalSalaries = 0;
+    // loop through employees
+    for( let i =0; i< employees.length; i++ ){
+        // add employee's salary to total
+        totalSalaries += Number( employees[ i ].annualSalary );
+    } // end for
+    // divide total salaries by 12
+    const totalMonthlySalaries = totalSalaries / 12;
+    console.log( 'totalMonthlySalaries:', totalMonthlySalaries );
+    // update Monthly salaries on DOM
+    let el = $( '#monthlyOut' );
+    el.empty();
+    el.append( totalMonthlySalaries.toFixed( 2 ) );
+    // update DOM
 } // end add Employee
 
 function readyNow(){
